e9300a7b9551d8aa194dac3d6549bdf9d1065249
AirBurn <rosan1233456@gmail.com>
2025-04-21 12:23:28 +0300

    no action items fix

--- src/MonkeFavoritesMod/Helpers/Commands.cs
+++ src/MonkeFavoritesMod/Helpers/Commands.cs
@@ -7,4 +7,32 @@ class Commands
         AddFavorite = 101,
         RemoveFavorite = 102;
 
+    public static void SetupModCommands(string? Id)
+    {
+        if(Id is null)
+            return;
+        CommonContextMenu contextMenu = UI.Get<CommonContextMenu>();
+        if (!FavoritesHelper.ShouldBeMarked(Id)) 
+            contextMenu.SetupCommand(Localization.Get($"ui.context.{nameof(AddFavorite)}"), AddFavorite);
+        else
+            contextMenu.SetupCommand(Localization.Get($"ui.context.{nameof(RemoveFavorite)}"), RemoveFavorite);
+        if(!UI.IsShowing<CommonContextMenu>()) {
+            UI.Chain<CommonContextMenu>().Show(true).SetBackgroundOrder(-1).SetBackOnBackgroundClick(true);
+            return;
+        }
+        contextMenu.InitSize(0);
+    }
+
+    public static bool ExecuteModCommands(string? Id, int bindValue)
+    {
+        if(Id is null)
+            return true;
+        switch (bindValue)
+        {
+            default: return true;
+            case AddFavorite: FavoritesHelper.AddFavorite(Id); return false;
+            case RemoveFavorite: FavoritesHelper.RemoveFavorite(Id); return false;
+        }
+    }
+
 }
--- src/MonkeFavoritesMod/Patches/InventoryScreenPatch.cs
+++ src/MonkeFavoritesMod/Patches/InventoryScreenPatch.cs
@@ -10,29 +10,13 @@ class InventoryScreenPatch
     [HarmonyPatch("DragControllerShowContextMenuCallback"), HarmonyPostfix]
     static void DragControllerShowContextMenuCallbackPostfix(ItemSlot obj)
     {
-        string? Id = obj?.Item?.Id as string;
-        if(Id is null)
-            return;
-        CommonContextMenu contextMenu = UI.Get<CommonContextMenu>();
-        if (!FavoritesHelper.ShouldBeMarked(Id)) 
-            contextMenu.SetupCommand(Localization.Get($"ui.context.{nameof(Commands.AddFavorite)}"), Commands.AddFavorite);
-        else
-            contextMenu.SetupCommand(Localization.Get($"ui.context.{nameof(Commands.RemoveFavorite)}"), Commands.RemoveFavorite);
-        contextMenu.InitSize(0);
+        Commands.SetupModCommands(obj?.Item?.Id as string);
     }
 
     [HarmonyPatch("ContextMenuOnCmdSelected"), HarmonyPrefix]
-    static void ContextMenuOnCmdSelectedPrefix(InventoryScreen __instance, int bindValue)
+    static bool ContextMenuOnCmdSelectedPrefix(InventoryScreen __instance, int bindValue)
     {
-        string? Id = __instance?._contextMenuItemSlot?.Item?.Id as string;
-        if(Id is null)
-            return;
-        switch (bindValue)
-        {
-            default: return;
-            case Commands.AddFavorite: FavoritesHelper.AddFavorite(Id); break;
-            case Commands.RemoveFavorite: FavoritesHelper.RemoveFavorite(Id); break;
-        }
+        return Commands.ExecuteModCommands(__instance?._contextMenuItemSlot?.Item?.Id as string, bindValue);
     }
 
 }
